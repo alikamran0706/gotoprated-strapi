d8378e06cc1102d8938cd8a4a32f65964d9f9c40
alikamran0706 <alikamrantechdev@gmail.com>
2026-07-25 18:14:23 -0700

    update company table

--- src/api/company-location/routes/company-location.ts
+++
@@ -1,7 +0,0 @@
-/**
- * company-location router
- */
-
-import { factories } from '@strapi/strapi';
-
-export default factories.createCoreRouter('api::company-location.company-location');
--- src/api/company-location/services/company-location.ts
+++
@@ -1,7 +0,0 @@
-/**
- * company-location service
- */
-
-import { factories } from '@strapi/strapi';
-
-export default factories.createCoreService('api::company-location.company-location');
