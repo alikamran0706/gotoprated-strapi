deb37b345281d6d848326703564c43ab16332f04
alikamran0706 <alikamrantechdev@gmail.com>
2026-08-10 14:57:29 +0500

    changes

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -5,7 +5,13 @@ export default [
   {
     name: 'strapi::cors',
     config: {
-      origin: '*', 
+      origin: [
+        'http://localhost:3000',
+        'https://gotoprated.com',
+        'https://www.gotoprated.com',
+        'https://www.y11r02bk4czqfk5rmmbs7xax.5.78.100.153.sslip.io',
+        'http://y11r02bk4czqfk5rmmbs7xax.5.78.100.153.sslip.io',
+      ],
       headers: ['Content-Type', 'Authorization', 'X-App-Version'],
       methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
       credentials: true,
